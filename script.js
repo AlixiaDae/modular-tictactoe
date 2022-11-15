aa59862f7f0c36c26f99f027f64bb1374950a438
AlixiaDae <lizziejones1208@gmail.com>
2022-11-16 00:09:38 +0800

    Add check if cell is empty
    Check cell if empty and prevents the turn from being switched to computer or player.

--- script.js
+++ script.js
@@ -6,29 +6,36 @@ const Player = (marker) => {
     }
 }
 
-const displayController = (() => {
+const gameDisplay = (() => {
     const playerOne = Player('X');
     const computerOne = Player('O');
     let playerTurn = true;
+    let gameOver = 9;
 
     function availableCell() {
         return gameCells.filter((cell) => cell.textContent === '');
     }
 
     const placePlayerToken = (cell) => {
-        if(playerTurn) {
+        if (gameOver == 0) {
+            alert('No more turns available!');
+        } else if(cell.target.textContent == `${playerOne.playerMarker()}`) {
+            console.log('Cell taken by player token!')
+        } else if (cell.target.textContent ==  `${computerOne.playerMarker()}`) { 
+            console.log('Cell taken by computer token!')
+        } else if(playerTurn && gameOver > 0) {
             cell.target.textContent = `${playerOne.playerMarker()}`;
             playerTurn = !playerTurn;
-        } else if(!playerTurn) {
-        alert(`Computer's turn`)
+            gameOver--;
+        } else if(!playerTurn && gameOver > 0) {
+            availableCell()[0].textContent = `${computerOne.playerMarker()}`;
+            playerTurn = !playerTurn
+            gameOver--;
     }
 }
-
     const gameCells = [...document.querySelectorAll('.cell')];
-
     gameCells.forEach(cell => {
         cell.addEventListener('click', placePlayerToken)
     })
-
 
 })()
