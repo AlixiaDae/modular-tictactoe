b5e2907ab08def0f163f21f24e94030c926b5006
AlixiaDae <lizziejones1208@gmail.com>
2022-11-16 01:16:29 +0800

    Add checkRows winning condition

--- script.js
+++ script.js
@@ -11,7 +11,18 @@ const gameDisplay = (() => {
     const computerOne = Player('O');
     let playerTurn = true;
     let gameOver = 9;
-
+    const gameCells = [...document.querySelectorAll('.cell')];
+    const winningCombination = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    
     function availableCell() {
         return gameCells.filter((cell) => cell.textContent === '');
     }
@@ -33,9 +44,39 @@ const gameDisplay = (() => {
             gameOver--;
     }
 }
-    const gameCells = [...document.querySelectorAll('.cell')];
+      const checkRows = () => {  gameCells.forEach(() => {
+           if(gameCells[0].textContent === 'O' && gameCells[1].textContent === 'O' && gameCells[2].textContent === 'O') {
+            console.log('computer wins!');
+            gameOver = 0
+           } else if (gameCells[3].textContent === 'O' && gameCells[4].textContent === 'O' && gameCells[5].textContent === 'O') {
+            console.log('computer wins!');
+            gameOver = 0
+           } else if (gameCells[6].textContent === 'O' && gameCells[7].textContent === 'O' && gameCells[8].textContent === 'O') {
+            console.log('computer wins!');
+            gameOver = 0
+           } else if(gameCells[0].textContent === 'X' && gameCells[1].textContent === 'X' && gameCells[2].textContent === 'X') {
+            console.log('Player wins!');
+            gameOver = 0
+           } else if (gameCells[3].textContent === 'X' && gameCells[4].textContent === 'X' && gameCells[5].textContent === 'X') {
+            console.log('Player wins!');
+            gameOver = 0
+           } else if (gameCells[6].textContent === 'X' && gameCells[7].textContent === 'X' && gameCells[8].textContent === 'X') {
+            console.log('Player wins!');
+            gameOver = 0
+    }}
+)}
+
+    
     gameCells.forEach(cell => {
         cell.addEventListener('click', placePlayerToken)
     })
+
+    gameCells.forEach(cell => {
+        cell.addEventListener('click', checkRows)
+    })
+
+    return {
+        
+    }
 
 })()
